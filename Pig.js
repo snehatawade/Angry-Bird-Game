7b35b3fa8f95f05b74ca2fea14a2c60939878531
snehatawade <54199703+snehatawade@users.noreply.github.com>
2019-10-22 18:07:07 +0530

    Add files via upload

--- Pig.js
+++ Pig.js
@@ -1,37 +1,22 @@
-class Pig extends BaseClass{
-    constructor(x, y) {
-      super(x,y,50,50);
-      this.image = loadImage("sprites/sprites/enemy.png");
-
-    }
-    display(){
-    super.display();
-  }
-}
-/*class Pig {
-  constructor(x, y) {
-    var options = {
-        'restitution':0.8,
-        'friction':0.3,
-        'density':1.0
-    }
-    this.body = Bodies.rectangle(x, y, 50, 50, options);
-    this.width = 50;
-    this.height = 50;
-    
-    World.add(world, this.body);
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+    this.visibility = 255;
   }
   display(){
-    var pos =this.body.position;
-    var angle = this.body.angle;
-    push();
-    translate(pos.x, pos.y);
-    rotate(angle);
-    rectMode(CENTER);
-
-    fill("green");
-    rect(0, 0, this.width, this.height);
-    pop();
+    console.log(this.body.speed);
+  if(this.body.speed<3){
+    super.display();
   }
-};*/
-     
+  else{
+  World.remove(world,this.body);
+  push();
+  this.visibility = this.visibility-5;
+  tint(255,this.visibility);
+  image(this.image,this.body.position.x,this.body.position.y,50,50);
+  pop();
+  }
+}
+
+};
